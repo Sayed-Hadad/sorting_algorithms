75da276a280c03cbc3fc523352d2059cb06d9a47
sayed elhadad <anasesozaking@gmail.com>
2024-01-24 03:00:37 +0200

    Done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,56 @@
 #include "sort.h"
 
 /**
- * swap - swaps 2 numbers
- * @a: the first number
- * @b: the second number
- * Return: void
-*/
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * Description: This function swaps the values of the two integers
+ * pointed to by @a and @b.
+ */
 void swap(int *a, int *b)
 {
-	int temp;
-
-	temp = *a;
+	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
-/**
- * bubble_sort - sorts an array using bubble sort algorithm
- * @array: the array to sort
- * @size: the size of the array
- * Return: void
-*/
 
+/**
+ * bubble_sort - Sorts an array using the bubble sort algorithm.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ *
+ * Description: This function implements the bubble sort algorithm
+ * to sort the elements in ascending order. It iterates through the
+ * array, comparing adjacent elements and swapping them if they are
+ * in the wrong order. The process repeats until the entire array
+ * is sorted.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < (size - i - 1); j++)
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++)
 		{
+			/* If the next element is smaller, swap them */
 			if (array[j + 1] < array[j])
 			{
 				swap(&array[j + 1], &array[j]);
+				swapped = 1;
 				print_array(array, size);
 			}
+		}
+
+		/* If no swapping occurred, the array is already sorted */
+		if (swapped == 0)
+		{
+			/* Break the loop as the array is sorted */
+			break;
 		}
 	}
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * Description: This function swaps the values of the two integers
+ * pointed to by @a and @b.
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, minIndex;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        minIndex = i;
+
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            swap(&array[i], &array[minIndex]);
+            print_array(array, size);
+        }
+    }
+}
